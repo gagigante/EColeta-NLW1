a9ceb02250fc72891005065871c2b5d160b2c428
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-06-07 15:44:46 -0300

    Fourth day

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -8,8 +8,8 @@ class ItemsController {
     const serializedItems = items.map(item => {
       return {
         id: item.id,
-        title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        title: item.title ,
+        image_url: `http://192.168.0.105:3333/uploads/${item.image}`,
       };
     });
   
