0b8bcaf0465dac3e778a0e2f4147b522cf88ea9d
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-06-07 11:50:55 -0300

    Third day

--- server/src/database/seeds/createItems.ts
+++ server/src/database/seeds/createItems.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
   await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e baterias', image: 'baterias.svg' },
-    { title: 'Papéis e papelão', image: 'papeis-papelão.svg' },
+    { title: 'Papéis e papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos orgânicos', image: 'organicos.svg' },
     { title: 'Óleo de cozinha', image: 'oleo.svg' },
